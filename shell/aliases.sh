a765070b9b24f0ecef6edf4eb475040581302e6f
Leviakc <gomen98.jg@gmail.com>
2024-02-15 20:36:39 -0600

    dotfiles changed

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,74 +3,17 @@
 alias cp="cp -i"                          # confirm before overwriting something
 alias df='df -h'                          # human-readable sizes
 alias more=less
-alias neofetch="neofetch --config $HOME/Documentos/config1.conf --ascii $HOME/Documentos/locky-ascii.txt"
-alias cda='cd ~/Documentos/academics'
-alias colta="cd ~/Documentos/cursos/colt/algrithm_data_structure"
-alias coltd="cd ~/Documentos/cursos/colt/database"
-alias cur="cd ~/Documentos/cursos"
-alias docs="cd ~/Documentos/"
-alias ferd='cd ~/Documentos/cursos/fernando/deno-node/'
-alias fern='cd ~/Documentos/cursos/fernando/node/'
-alias ferr='cd ~/Documentos/cursos/fernando/react/'
-alias ferf='cd ~/Documentos/cursos/fernando/flutter'
-alias ferrn='cd ~/Documentos/cursos/fernando/react-native'
-alias ferrp='cd ~/Documentos/cursos/fernando/react-pro'
-alias jonas='cd ~/Documentos/cursos/jonas/'
-alias jonasa='cd ~/Documentos/cursos/jonas/advanced-css'
-alias jonashc='cd ~/Documentos/cursos/jonas/html-css'
-alias jonasj='cd ~/Documentos/cursos/jonas/javascript'
-alias jonasn='cd ~/Documentos/cursos/jonas/node'
-alias jonasr='cd ~/Documentos/cursos/jonas/react'
-alias jordan="cd ~/Documentos/cursos/jordan"
-alias cdl="cd ~/Documentos/cursos/Linux"
-alias cdlua='cd ~/Documentos/cursos/lua'
-alias cdmaxt="cd ~/Documentos/cursos/maximilian/typescript"
-#alias cdmaxd="cd ~/Documentos/cursos/maximilian/django"
-alias midug='cd ~/Documentos/cursos/midu/git'
-alias midun='cd ~/Documentos/cursos/midu/node'
-alias midur='cd ~/Documentos/cursos/midu/react'
-alias midut='cd ~/Documentos/cursos/midu/typescript'
-alias mircha="cd ~/Documentos/cursos/mircha/"
-alias mirchahc="cd ~/Documentos/cursos/mircha/html-css"
-alias mirchaj="cd ~/Documentos/cursos/jonmircha/js"
-alias cdn='cd ~/Documentos/cursos/js/nodejs/'
-alias cdp='cd ~/Documentos/cursos/python'
-alias personal='cd ~/Documentos/proyectos-personales'
-alias cdr="cd ~/Documentos/cursos/rust/"
-alias stephend="cd ~/Documentos/cursos/stephen/database"
-alias stephent="cd ~/Documentos/cursos/stephen/typescript"
-alias gitbook='open ~/Descargas/books/aprendiendo-git.pdf'
-alias horario='open ~/Descargas/horario.pdf'
-alias ingles="cd ~/Documentos/cursos/ingles"
-alias luabook='open ~/Descargas/books/Programming-in-Lua.pdf'
-alias maria='mysql -u root -p'
-alias open="xdg-open"
-alias padd='pnpm add'
-alias pdev='pnpm dev --host'
-# alias pgserver='cd; cd pgadmin4; cd bin; ./pgadmin4'
-alias pinit='pnpm init'
-alias pvite='pnpm create vite'
-alias postman='cd ~/Descargas/Postman ; ./Postman'
+alias neofetch="neofetch --config $HOME/dotfiles/config1.conf --ascii $HOME/dotfiles/locky-ascii.txt"
 alias sudovi='sudo -E nvim'
 alias tarc="tar -cvzf"
 alias tard="tar -xvzf"
-alias tor='cd ~/Descargas/tor-browser_en-US/ ; ./start-tor-browser.desktop'
-alias zshrc='nvim ~/.zshrc'
-alias fconf='nl ~/.config/nvimLazy/lua/plugins/flutter.lua'
-alias nconf='nvim ~/.config/nvim/init.lua'
-alias nvconfig='cd ~/.config/nvim'
-alias pdlx='pnpm dlx'
-
-alias mysql='mariadb'
-alias /usr/bin/mysql='/usr/bin/mariadb'
-
 alias cd3="cd ../../.."
 alias cd2="cd ../.."
 alias tmuxs="tmux new -s"
 #alias vim="nvim"
 
-alias vim="NVIM_APPNAME=nvimLua nvim"
-alias nl="NVIM_APPNAME=nvimLazy nvim"
+# Last/Current directory
+alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 
 # scripts
 alias denoserver="~/.config/nvim/deno-node-server.ts"
